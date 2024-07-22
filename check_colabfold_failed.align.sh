0e1b90fe08ae27361cc28d1337a292bc28235a78
jflucier <3505568+jflucier@users.noreply.github.com>
2024-07-22 12:21:44 -0400

    patch chack align script

--- check_colabfold_failed.align.sh
+++ check_colabfold_failed.align.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
+check_op=$2
 base_path=$1
 
 echo "running on base path: $base_path"
-
 list=$(ls -d ${base_path}/*/)
 
-echo "Here is the list of failed align jobs:"
-for nn in $list
-do
-  n=$(basename ${nn})
-  if [ ! -f ${nn}/0.a3m ]; then
-    g=$(grep -e ${n} 01_submit_all_colab_search_jobs.sh)
-    echo "${g}"
-  fi
-done
+if [[ "$check_op" == "align" ]]; then
+  echo "Here is the list of failed align jobs:"
+  for nn in $list
+  do
+    n=$(basename ${nn})
+    if [ ! -f ${nn}/0.a3m ]; then
+      g=$(grep -e ${n} ${base_path}/01_submit_all_colab_search_jobs.sh)
+      echo "${g}"
+    fi
+  done
+elif [[ "$check_op" == "fold" ]]; then
+  echo "Here is the list of failed fold jobs:"
+  for nn in $list
+  do
+    n=$(basename ${nn})
+    if [ ! -f ${nn}/${n}.zip ]; then
+      g=$(grep -e ${n} ${base_path}/02_submit_all_colab_fold_jobs.sh)
+      echo "${g}"
+    fi
+  done
+else
+   echo "Unrecongnised check operation. Possible values are: align or fold"
+fi
 
-# detect failed fold jobs
-list=$(ls -d ${base_path}/*/)
-echo ""
-echo ""
-echo "Here is the list of failed fold jobs:"
-for nn in $list
-do
-  n=$(basename ${nn})
-  if [ ! -f ${nn}/${n}.zip ]; then
-#    echo "$n"
-    g=$(grep -e ${n} 02_submit_all_colab_fold_jobs.sh)
-    echo "${g}"
-  fi
-done
 
 
