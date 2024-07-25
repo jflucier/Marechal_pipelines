98a1c045ef5b716a69d0e653db8d8a180ec97f45
jflucier <3505568+jflucier@users.noreply.github.com>
2024-07-25 10:01:00 -0400

    patch chack align script

--- check_colabfold_failed.sh
+++ check_colabfold_failed.sh
@@ -3,11 +3,11 @@
 check_op=$1
 base_path=$2
 
-echo "running on base path: $base_path"
+echo "#running on base path: $base_path"
 list=$(ls -d ${base_path}/*/)
 
 if [[ "$check_op" == "align" ]]; then
-  echo "Here is the list of failed align jobs:"
+  echo "#Here is the list of failed align jobs:"
   for nn in $list
   do
     n=$(basename ${nn})
@@ -17,11 +17,11 @@ if [[ "$check_op" == "align" ]]; then
     fi
   done
 elif [[ "$check_op" == "fold" ]]; then
-  echo "Here is the list of failed fold jobs:"
+  echo "#Here is the list of failed fold jobs:"
   for nn in $list
   do
     n=$(basename ${nn})
-    if [ ! -f ${nn}/${n}.zip ]; then
+    if [ ! -f ${base_path}/${n}.zip ]; then
       g=$(grep -e ${n} ${base_path}/02_submit_all_colab_fold_jobs.sh)
       echo "${g}"
     fi
