82e803cefe16227a9a05d3aaba61a88ea488b00c
jflucier <3505568+jflucier@users.noreply.github.com>
2025-01-27 14:38:38 -0500

    depmap correlation

--- depmap/depmap_correlation.py
+++ depmap/depmap_correlation.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 print("Reading depmap data")
 df = pd.read_csv(
-    "/storage/Documents/service/biologie/marechal/analysis/20250127_ligasee3_depmap/OmicsExpressionProteinCodingGenesTPMLogp1BatchCorrected.csv",
+    "/home/def-marechal/analysis/20250127_ligasee3_depmap/OmicsExpressionProteinCodingGenesTPMLogp1BatchCorrected.csv",
     index_col=0,
     sep=","
 )
@@ -14,7 +14,7 @@ df2=df.corr()
 
 print("Output correlation")
 df2.to_csv(
-    "/storage/Documents/service/biologie/marechal/analysis/20250127_ligasee3_depmap/depmap_correlation.tsv",
+    "/home/def-marechal/analysis/20250127_ligasee3_depmap/OmicsExpressionProteinCodingGenesTPMLogp1BatchCorrected.correlation.tsv",
     sep="\t"
 )
 
