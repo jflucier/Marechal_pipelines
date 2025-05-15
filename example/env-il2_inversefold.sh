0285c8b92595cc595b1beaa8743f93c89a7dd978
Maxime <maxime.levesque@gmail.com>
2025-05-15 11:18:09 -0400

    ...

--- example/env-il2_inversefold.sh
+++ example/env-il2_inversefold.sh
@@ -7,9 +7,6 @@
 export PYTHONPATH=$(dirname $PWD)/DPfold
 
 
-export DPFOLD_REMOTE_SSH_HOST_DIR=gh1301:
-
-
 export DPFOLD_REMOTE_SSH_HOST_DIR=gh1301:/tank/maxl
 export DRYPIPE_PIPELINE_GENERATOR="dpfold.dag:openfold_pipeline"
 export DRYPIPE_PIPELINE_INSTANCE_DIR=$1
