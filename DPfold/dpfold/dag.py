5120149f7371e066b04d9432b4a7946df1895be9
Maxime <maxime.levesque@gmail.com>
2025-09-12 15:04:57 -0400

    ..

--- DPfold/dpfold/dag.py
+++ DPfold/dpfold/dag.py
@@ -165,7 +165,7 @@ def collabfold_dag(dsl, multimer_batch, samplesheet, collabfold_task_conf_func):
             colabfold_search_task = dsl.task(
                 key=f"cf-fold.{multimer_name}",
                 is_slurm_array_child=True,
-                task_conf=TaskConf(extra_env=tc.extra_env())
+                task_conf=TaskConf(extra_env=tc.extra_env)
             ).inputs(
                 samplesheet=dsl.file(samplesheet),
                 multimer_name=multimer_name,
